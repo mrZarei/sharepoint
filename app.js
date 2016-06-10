816364396c351e141722b33e11cdf187c183b400
Mohammadreza <zarei.mohamadreza@gmail.com>
2016-06-10 10:24:31 +0200

    init

---
+++ app.js
@@ -0,0 +1,37 @@
+function getIdFromUri(ff) {
+    var scriptbase = hostweburl + "/_layouts/15/";
+    // console.log(ff);
+    // Load the js files and continue to the successHandler
+    $.getScript(scriptbase + "SP.RequestExecutor.js", function () { execCrossDomainRequestIdFromUri(ff); });
+}
+function execCrossDomainRequestIdFromUri(ff) {
+    for (index in ff) {
+        var executor = new SP.RequestExecutor(appweburl);
+        executor.executeAsync(
+           {
+               
+               url: ff[index].uri,
+               method: "GET",
+               headers: { "Accept": "application/json; odata=verbose" },
+               success:  function (data, textStatus, xhr) {
+                   var jsonObject = JSON.parse(data.body);
+                   var results = jsonObject.d; var item = 0; var i = 0;
+                   console.log(results);
+                   arrayOfId.push(results.Id,ff[index].Name);
+                  
+                   console.log(arrayOfId);
+
+
+                   if (arrayOfId.length == ff.length) {
+                       if (index == ff.length - 1) {
+                           console.log(arrayOfId);
+                           getArraysOfIds.resolve(arrayOfId);
+
+                       }
+                   }
+               },
+               error: errorHandlerIdFromUri
+           }
+       );
+    }
+}
