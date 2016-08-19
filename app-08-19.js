b36d74800f44ef75f04f5e444f47ffe3e0b17694
Mohammadreza <zarei.mohamadreza@gmail.com>
2016-08-19 15:42:55 +0200

    update for loop

--- app-08-19.js
+++ app-08-19.js
@@ -5,7 +5,28 @@
 
 for (var i=0;i<header.length;) {
     var parameter=header[i];
-    var pramob=new myobjec  (splitArray[0], splitArray[1],parameter);
+    var site = splitArray[0];
+    var list = splitArray[1];
+    var execut = new SP.RequestExecutor(appweburl);
+    execut.executeAsync(
+        {
+            url: appweburl + "/_api/SP.AppContextSite(@target)/web/lists/getbytitle('testing')/items?$select=" + paramet + "&@target='" + hostweburl + "'",
+            MyParameter1: site,
+            MyParameter2: list,
+            THTable: parameter,
+            method: "GET",
+            headers: { "Accept": "application/json; odata=verbose" },
+            success: function(data){
+                console.log(this.MyParameter1);
+                console.log(data);
+                jsonObject = JSON.parse(data.body);
+                console.log(jsonObject.d)
+                var result = jsonObject.d.results;
+                console.log(result);
+            },
+            error: errorHandleToRetreiveItems
+        }
+    );
     pramob.excute();
     i++;
 }
@@ -28,29 +49,7 @@ var myobjec = function (site, list, paramet) {
 
 }
 
-/////////////////////
-function execCrossDomainRequestToRetreiveItemFromList(site, list, paramet) {
-    var execut = new SP.RequestExecutor(appweburl);
-    execut.executeAsync(
-        {
-            url: appweburl + "/_api/SP.AppContextSite(@target)/web/lists/getbytitle('testing')/items?$select=" + paramet + "&@target='" + hostweburl + "'",
-            MyParameter1: site,
-            MyParameter2: list,
-            THTable: paramet,
-            method: "GET",
-            headers: { "Accept": "application/json; odata=verbose" },
-            success: function(data){
-                console.log(this.MyParameter1);
-                console.log(data);
-                jsonObject = JSON.parse(data.body);
-                console.log(jsonObject.d)
-                var result = jsonObject.d.results;
-                console.log(result);
-            },
-            error: errorHandleToRetreiveItems
-        }
-    );
-}
+
 
 
 
